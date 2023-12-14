4cecc243c3c5925424bf507d0cfe1e682a398df8
aditya <adisadis1234@gmail.com>
2023-12-14 15:15:51 +0530

    working on project

--- src/Component/Project/Project.js
+++ src/Component/Project/Project.js
@@ -1,12 +1,24 @@
 import { Box, Typography, styled } from "@material-ui/core";
 import React from "react";
-import { Images } from "../../Constant";
+import { Images, projectData } from "../../Constant";
 
 export default function Project() {
+  const renderServicesUpper = () => {
+    return (
+      <OurProjectBox>
+        <AccomplishmentTxt>ACCOMPLISHMENTS</AccomplishmentTxt>
+        <OurProjectTxt className="myServicesTxt">Our Projects</OurProjectTxt>
+        <OurProjectTxtDescription className="servicesDescription">
+          Far far away, behind the word mountains, far from the countries
+          Vokalia and Consonantia
+        </OurProjectTxtDescription>
+      </OurProjectBox>
+    );
+  };
   const renderContact = () => {
     return (
       <MainBox>
-        <Box>
+        <InnerBox>
           <ProjectTxt>Have a project on your mind.</ProjectTxt>
           <ProjectTxtDescription>
             A small river named Duden flows by their place and supplies it with
@@ -16,7 +28,7 @@ export default function Project() {
           <ContactButton variant="contained" color="success">
             Contact Me
           </ContactButton>
-        </Box>
+        </InnerBox>
         <img
           src={Images.projectImgUser}
           alt="projectImgUser"
@@ -31,15 +43,38 @@ export default function Project() {
       </MainBox>
     );
   };
-  return <Box id="Projects">{renderContact}</Box>;
+
+  const renderProjectCard = () => {
+    return (
+      <CardBox>
+        {projectData.map((item, index) => (
+          <InnerCardBox key={index}>
+            <img
+              src={item.img}
+              style={{ height: "100%", width: "100%", borderRadius: "7px" }}
+            />
+          </InnerCardBox>
+        ))}
+      </CardBox>
+    );
+  };
+
+  return (
+    <Box id="Projects">
+      {renderContact()}
+      {renderServicesUpper()}
+      {renderProjectCard()}
+    </Box>
+  );
 }
 
 const MainBox = styled(Box)({
   backgroundColor: "#b1b492",
-  height: "200px",
+  height: "250px",
   display: "flex",
   flexDirection: "row",
   position: "relative",
+  padding: "0px 90px",
 });
 
 const ContactButton = styled(Box)({
@@ -66,4 +101,59 @@ const ProjectTxtDescription = styled(Typography)({
   color: "#fff",
   fontWeight: 500,
   fontSize: "17px",
+});
+
+const InnerBox = styled(Box)({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-evenly",
+});
+
+const AccomplishmentTxt = styled(Typography)({
+  color: "#b1b492",
+  fontWeight: 800,
+  fontSize: "15px",
+  marginBottom: "15px",
+});
+
+const OurProjectBox = styled(Box)({
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  flexDirection: "column",
+  margin: "60px 0px",
+  padding: "0px 20px",
+});
+
+const OurProjectTxtDescription = styled(Typography)({
+  color: "#999999",
+  fontWeight: 600,
+  fontSize: "16px",
+});
+
+const OurProjectTxt = styled(Typography)({
+  color: "#000",
+  fontWeight: 800,
+  fontSize: "35px",
+  marginBottom: "25px",
+});
+const CardBox = styled(Box)({
+  display: "flex",
+  gap: "30px",
+  margin: "0px 30px",
+  flexWrap: "wrap",
+});
+
+const InnerCardBox = styled(Box)({
+  // border: "1px solid red",
+  width: "320px",
+  height: "320px",
+  // borderRadius: "7px",
+  justifyContent: "center",
+  transition: "transform 0.3s ease-in-out",
+  "&:hover": {
+    // borderColor: "red",
+    // transform: "scale(1.1)", // Scaling the card on hover
+    // boxShadow: "0px 0px 10px 0px rgba(0, 0, 0, 0.2)", // Adding a box-shadow on hover
+  },
 });
--- src/Constant.js
+++ src/Constant.js
@@ -9,6 +9,7 @@ import movieImg from "../src/Assets/movieImg.png";
 import musicImg from "../src/Assets/musicImg.png";
 import travelImg from "../src/Assets/travelImg.png";
 import projectImgUser from "../src/Assets/projectImgUser.png";
+import projectOneImg from "../src/Assets/projectOneImg.png";
 
 const navbarData = [
   { id: 1, title: "HOME", hrefKey: "#Home" },
@@ -26,6 +27,7 @@ const Images = {
   projectImg: projectImg,
   aboutImg: aboutImg,
   projectImgUser: projectImgUser,
+  projectOneImg: projectOneImg,
 };
 const statsData = [
   { id: 1, title: "PROJECT COMPLETE", img: Images.clientImg, numbers: 5 },
@@ -133,13 +135,12 @@ const servicesData = [
       "A small river named Duden flows by their place and supplies.",
     serviceImg: musicImg,
   },
-  {
-    id: 8,
-    serviceTitle: "SEO",
-    serviceDescriptions:
-      "A small river named Duden flows by their place and supplies.",
-    serviceImg: musicImg,
-  },
+];
+const projectData = [
+  { id: 1, title: "PROJECT COMPLETE", img: Images.projectOneImg, numbers: 5 },
+  { id: 2, title: "HAPPY CLIENTS", img: Images.projectImg, numbers: 3 },
+  { id: 3, title: "CUPS OF COFFEE", img: Images.clientImg, numbers: 10 },
+  { id: 4, title: "YEARS EXPERIENCED", img: Images.projectImg, numbers: 3 },
 ];
 export {
   navbarData,
@@ -149,4 +150,5 @@ export {
   hobbyData,
   skillsData,
   servicesData,
+  projectData,
 };
